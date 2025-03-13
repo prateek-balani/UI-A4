ae53727352d8b555219df681ec182b2b167d128d
prateek-balani <prateekbalani03@gmail.com>
2025-03-12 17:46:40 -0700

    scripts update

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -6,12 +6,17 @@ public class GameManager : SingletonMonoBehavior<GameManager>
     [SerializeField] private int score = 0;
     [SerializeField] private TextMeshProUGUI scoreText;
     [SerializeField] private CoinCounterUI coinCounter;
+    [SerializeField] private InputManager inputManager;
+    [SerializeField] private GameObject settingsMenu;
+    private bool isSettingsMenuActive;
+    public bool IsSettingsMenuActive => isSettingsMenuActive;
 
     protected override void Awake()
     {
         base.Awake();
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        inputManager.OnSettingsMenu.AddListener(ToggleSettingsMenu);
 
         if (coinCounter == null)
         {
@@ -22,6 +27,7 @@ public class GameManager : SingletonMonoBehavior<GameManager>
         {
             Debug.LogWarning("ScoreText is not assigned in the GameManager.");
         }
+        DisableSettingsMenu();
     }
 
     public void IncreaseScore()
@@ -29,6 +35,36 @@ public class GameManager : SingletonMonoBehavior<GameManager>
         score++;
         coinCounter.UpdateScore(score);
         scoreText.text = $"Score: {score}";
-        
+
+    }
+
+    private void ToggleSettingsMenu()
+    {
+        if (isSettingsMenuActive)
+        {
+            DisableSettingsMenu();
+        }
+        else
+        {
+            EnableSettingsMenu();
+        }
+    }
+
+    private void EnableSettingsMenu()
+    {
+        Time.timeScale = 0f;
+        settingsMenu.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        isSettingsMenuActive = true;
+    }
+
+    private void DisableSettingsMenu()
+    {
+        Time.timeScale = 1f;
+        settingsMenu.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        isSettingsMenuActive = false;
     }
 }
